97b486f94b24e38972d7db4a6a76e803f53f140c
DerVerruckteFuchs <derverrucktefuchs@protonmail.ch>
2019-05-28 09:15:27 -0400

    clean up config

--- keyboards/planck/keymaps/fuchs/keymap_punctuation.h
+++
@@ -1,9 +0,0 @@
-#ifndef KEYMAP_PUNCTUATION
-#define KEYMAP_PUNCTUATION
-
-#include "keymap.h"
-
-#define INBANG UC(0x203D) // ‽, interrobang
-#define PERCON UC(0x2E2E) // ⸮, percontation mark, irony mark, sarcasm mark
-
-#endif
